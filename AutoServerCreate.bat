30fe0c098a01a6dbc0a682ba0365d217f7a5854b
Maoyue <95519633+MagicTeaMC@users.noreply.github.com>
2023-02-22 16:20:48 +0800

    Update AutoServerCreate.bat

--- AutoServerCreate.bat
+++ AutoServerCreate.bat
@@ -1,7 +1,7 @@
 color B
 @echo off
 echo:
-echo       正在安裝伺服器檔案 [server.jar]
+echo       開始安裝伺服器 .jar [purpur.jar]
 echo:
 echo:                                                     
 echo:                       
@@ -13,42 +13,42 @@ echo:
 :labe50
 cd %~dp0
 curl -O https://api.purpurmc.org/v2/purpur/1.19.3/latest/download
-ren *.jar server.jar
+ren *.jar purpur.jar
 :labe51
 @echo off
-rem 設定title
-title=Minecraft Server auto set
+rem setting title
+title=Minecraft Server auto setup tool
 mode con cols=70 lines=30
 echo:
 echo:
 echo:
 echo:
 color B                
-echo                  server.jar檔案已安裝完成
+echo                  purpur.jar 下載完成
 echo:  
 echo:              
 echo:
 echo:
 pause
 cls
-if exist server.jar goto label2
-if not exist server.jar goto label1
+if exist purpur.jar goto label2
+if not exist purpur.jar goto label1
 :label1
 color 4
 echo   ERROR     
-echo         錯誤內容:未找到server.jar，請確認server.jar與此工具在同一個資料夾重試                                                           
+echo         ERROR: 找不到 purpur.jar，請確認 purpur.jar 與此工具在同一個資料夾重試                                                           
 color
 pause
 exit
 :label2
-echo = 請確保"Minecraft伺服器架設工具"與"server.jar"，放於同一資料夾 =
+echo = 請確認 purpur.jar，放於同一資料夾
 echo:
 echo:
-echo 一、檢查伺服器環境中
-ping -n 2 127.0.0.1 >NUL
+echo 1.正在檢查伺服器環境
+ping -n 2 127.0.0.1:25565 >NUL
 @echo off
 echo:
-echo 二、正在設定伺服器檔案
+echo 2.正在設定伺服器檔案
 if not exist StartServer.bat goto label4
 if exist 伺服器架設中.bat goto label3
 :label3
@@ -62,7 +62,7 @@ call StartServer.bat
 echo:
 ping -n 3 127.0.0.1 >NUL
 @echo off
-echo 三、伺服器檔案創建完成 "StartServer.bat"
+echo 伺服器檔案創建完成 "StartServer.bat"
 ping -n 3 127.0.0.1 >NUL
 @echo off
 echo:
@@ -96,7 +96,7 @@ rem 定義變數延遲環境，關閉回顯
 @echo off&setlocal enabledelayedexpansion
 rem 讀取eula.txt所有內容
 for /f "eol=* tokens=*" %%i in (eula.txt) do (
-rem 設定變數a為每行內容
+rem 變數a為每行內容
 set a=%%i
 rem 如果該行有false，則將其改為true
 set "a=!a:false=true!"
@@ -113,12 +113,12 @@ echo:
 echo: 
 echo:                                                             
 echo:                                                                  
-echo                          伺服器成功架設完成                
+echo                          伺服器設定成功                
 echo:                                                                 
 echo:                                                                  
 echo:   
 echo:                                                                  
-echo                           將自動啟動伺服器...                
+echo                           即將自動啟動伺服器...                
 echo:                                                                 
 echo:  
 echo:
