f258e939ca3abe96813b2f74023569a503218aa9
MagicTeaMC <oneowo87@gmail.com>
2024-08-24 13:55:43 +0800

    not stable also good

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     ", env!("CARGO_PKG_VERSION")));
 
     let mut project = String::new();
-    print!("Enter which server software do you want to use (paper/folia): ");
+    print!("Enter which server software do you want to use (paper/folia/purpur): ");
     io::stdout().flush()?;
     io::stdin().read_line(&mut project)?;
     let project = project.trim();
@@ -50,40 +50,54 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("Start downloading some essential files...");
 
-    let url = format!(
-        "https://api.papermc.io/v2/projects/{}/versions/{}/builds",
-        project, minecraft_version
-    );
-
     let client = Client::new();
-    let response = client.get(&url).send()?;
-    let json: Value = response.json()?;
 
-    let latest_build = json["builds"]
-        .as_array()
-        .ok_or("Unexpected JSON structure")?
-        .iter()
-        .filter(|build| build["channel"] == "default")
-        .filter_map(|build| build["build"].as_u64())
-        .max();
-
-    if let Some(build) = latest_build {
-        println!("Latest stable build is {}", build);
-        
-        let jar_name = format!("{}-{}-{}.jar", project, minecraft_version, build);
-        let download_url = format!(
-            "https://api.papermc.io/v2/projects/{}/versions/{}/builds/{}/downloads/{}",
-            project, minecraft_version, build, jar_name
+    if project == "purpur" {
+        let purpur_download_url = format!(
+            "https://api.purpurmc.org/v2/purpur/{}/latest/download",
+            minecraft_version
         );
 
-        println!("Downloading the latest {} version...", project);
-        let mut response = client.get(&download_url).send()?;
+        println!("Downloading the latest Purpur version...");
+        let mut response = client.get(&purpur_download_url).send()?;
         let mut file = File::create("server.jar")?;
         io::copy(&mut response, &mut file)?;
 
         println!("Download completed");
     } else {
-        println!("No stable build for version {} found :(", minecraft_version);
+        let url = format!(
+            "https://api.papermc.io/v2/projects/{}/versions/{}/builds",
+            project, minecraft_version
+        );
+
+        let response = client.get(&url).send()?;
+        let json: Value = response.json()?;
+
+        let latest_build = json["builds"]
+            .as_array()
+            .ok_or("Unexpected JSON structure")?
+            .iter()
+            .filter_map(|build| build["build"].as_u64())
+            .max();
+
+        if let Some(build) = latest_build {
+            println!("Latest build is {}", build);
+
+            let jar_name = format!("{}-{}-{}.jar", project, minecraft_version, build);
+            let paper_download_url = format!(
+                "https://api.papermc.io/v2/projects/{}/versions/{}/builds/{}/downloads/{}",
+                project, minecraft_version, build, jar_name
+            );
+
+            println!("Downloading the latest {} version...", project);
+            let mut response = client.get(&paper_download_url).send()?;
+            let mut file = File::create("server.jar")?;
+            io::copy(&mut response, &mut file)?;
+
+            println!("Download completed");
+        } else {
+            println!("No stable build for version {} found :(", minecraft_version);
+        }
     }
 
     Ok(())
