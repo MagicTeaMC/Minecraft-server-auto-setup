5f61245c67aa5d60136cbb3c7fe80d179f16b991
MagicTeaMC <oneowo87@gmail.com>
2025-07-10 00:33:09 +0800

    bump to 2.2.1

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ const CONFIG_FILE: &str = "mcsast.config.json";
 
 #[derive(Parser)]
 #[command(
-    version = "2.2.0",
+    version = "2.2.1",
     author = "Maoyue (MagicTeaMC)",
     about = "Manage Paper / Purpur / Folia / Velocity server quickly and easily!"
 )]
